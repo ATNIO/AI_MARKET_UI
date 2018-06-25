55aeb1978bce6b42bc313b6ca477386ea31ca055
junjia.wan <new_start@live.cn>
2018-06-25 15:54:47 +0800

    remove ts support

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,13 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/essential',
-    '@vue/prettier',
-    '@vue/typescript'
+    '@vue/prettier'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
   }
 }
