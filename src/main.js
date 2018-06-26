f1130f77dadeb7e0d5b9e3ea7b62b1a41239dd40
junjia.wan <new_start@live.cn>
2018-06-26 14:24:13 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import web3 from "./plugins/web3";
+
 Vue.config.productionTip = false;
+
+Vue.use(web3);
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  components: {},
+  mounted() {
+    // this._getBalance();
+  },
+  methods: {
+    async _getBalance() {
+      const { getCoinbase, getBalance } = this.$web3.eth;
+      const coinbase = await getCoinbase();
+      const balance = await getBalance(coinbase);
+
+      console.log(coinbase, balance);
+    }
   }
 };
 </script>
