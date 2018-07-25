d91fa06df8dc5c6141b862809d19eda4c4d59fe7
mimo <931750817@qq.com>
2018-07-25 11:11:43 +0800

    fluent

--- src/views/ListItemFluent.vue
+++ src/views/ListItemFluent.vue
@@ -1,6 +1,30 @@
 <template>
   <section class="ListItemFluent">
-    <h1>你不是</h1>
+    <el-row class="row">
+      <el-col v-for="(o, index) in 1" :key="o" :offset="index > 0 ? 2 : 0">
+        <el-card :body-style="{ padding: '0px' }" class="card">
+          <img src="../assets/icon-64.png" alt="" class="img">
+          
+          <span class="title">RANDOM FMAOUS QUOTES</span> 
+          <span class="describe">
+            <span>Get a random quote in JSON format.</span>
+            <span>Current categories are:- fa mous-movies.</span>
+          </span>  
+
+          <span class="provider"> by ANDRUXNET</span>
+            
+          <span class="price">FREE</span>
+
+          <div class="bottom clearfix">
+            <span class="icon-user"> 15442</span> 
+            <span class="icon-star"> 15202</span>
+            <span class="icon-arrow"> 100%</span>
+          </div>
+
+          <span class="icon-star star"></span>
+        </el-card>
+      </el-col>
+    </el-row>
     
   </section>
 </template>
@@ -10,3 +34,28 @@ export default {
   name: "ListItemFluent"
 };
 </script>
+
+<style lang="less" scoped>
+.row {
+  display: flex;
+  margin: 5em 5em;
+  .img {
+    flex: 1;
+  }
+  .title {
+    flex: 1;
+  }
+  .describe {
+    flex: 1;
+  }
+  .provider {
+    flex: 1;
+  }
+  .price {
+    flex: 1;
+  }
+  .bottom {
+    flex: 1;
+  }
+}
+</style>
--- src/views/ListItemGrid.vue
+++ src/views/ListItemGrid.vue
@@ -4,21 +4,22 @@
       <el-col v-for="(o, index) in 3" :key="o" :offset="index > 0 ? 2 : 0">
       <!-- <el-col> -->
         <el-card :body-style="{ padding: '0px' }" class="card">
+          <span class="icon-star star"></span><br>
           <span class="title">RANDOM FMAOUS QUOTES</span><br>
           <span class="provider">
             <span class="icon-user"></span>
-            <span>ANDRUXNET</span>
+            <span> ANDRUXNET</span>
           </span><br>
           <img src="../assets/icon-64.png" alt="">
-          <p>Get a random quote in JSON format.</p>
-          <p>Current categories are:- fa mous-movies.</p>
-          <div>
-            <span class="price">FREE</span>
-            <div class="bottom clearfix">
-                <span class="icon-user">number</span> 
-                <span class="icon-star">like</span>
-                <span class="icon-arrow">100%</span>
-            </div>
+          <div class="describe">
+            <p>Get a random quote in JSON format.</p>
+            <p>Current categories are:- fa mous-movies.</p>
+          </div>
+          <span class="price">FREE</span>
+          <div class="bottom clearfix">
+            <span class="icon-user"> 15442</span> 
+            <span class="icon-star"> 15202</span>
+            <span class="icon-arrow"> 100%</span>
           </div>
         </el-card>
       </el-col>
@@ -44,26 +45,54 @@ export default {
   .card {
     flex: 1;
     text-align: center;
+    .star {
+      background: linear-gradient(to bottom right, #a284f5, #4a86f4);
+      color: #ffffff;
+      float: right;
+      padding: 0.5em 0.5em;
+    }
     .title {
       color: #705db1;
       font-weight: 800;
       font-size: 1.3rem;
+      display: inline-block;
+      margin: 2em 0 0 0;
+    }
+    .provider {
+      display: inline-block;
+      margin: 2em 0;
+      color: #888888;
+    }
+    .describe {
+      font-size: 0.8rem;
+      color: #888888;
+      margin: 4em 0 2em 0;
+      p {
+        margin: 0.3em;
+      }
     }
     .price {
       width: 100%;
+      height: 2.5em;
+      line-height: 2.5em;
       display: block;
       text-align: center;
-      background: #bebffe;
+      background: linear-gradient(to right, #99c6fa, #bebffe);
       color: #ffffff;
-      font-size: 3rem;
+      font-size: 1.3rem;
     }
     .bottom {
       display: flex;
       background-color: #7f6fe7;
-
+      height: 2em;
+      color: #ffffff;
       span[class^="icon-"] {
+        line-height: 2em;
         flex: 1;
-
+        border-right: #ffffff 1px solid;
+        &:last-of-type {
+          border-style: none;
+        }
         &::before {
           color: #ffffff;
         }
