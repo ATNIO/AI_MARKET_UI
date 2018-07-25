ee8601bf75b08755e8c3044f077c7b6518612b45
mimo <931750817@qq.com>
2018-07-25 16:08:07 +0800

    grid

--- src/views/ListItemFluent.vue
+++ src/views/ListItemFluent.vue
@@ -1,24 +1,22 @@
 <template>
   <section class="ListItemFluent">
     <el-row class="row">
-      <el-col v-for="(o, index) in 1" :key="o" :offset="index > 0 ? 2 : 0">
+      <el-col>
         <el-card :body-style="{ padding: '0px' }" class="card">
-          <img src="../assets/icon-64.png" alt="" class="img">
+          <div class="img"><img src="../assets/icon-64.png" alt="" class="img"></div>
           
-          <span class="title">RANDOM FMAOUS QUOTES</span> 
-          <span class="describe">
-            <span>Get a random quote in JSON format.</span>
-            <span>Current categories are:- fa mous-movies.</span>
-          </span>  
-
-          <span class="provider"> by ANDRUXNET</span>
+          <div class="detail">
+            <h3 class="title">RANDOM FMAOUS QUOTES</h3> 
+            <p class="describe">Get a random quote in JSON format.Current categories are:- fa mous-movies.</p>  
+          </div>
+          <div class="provider"> by ANDRUXNET</div>
             
           <span class="price">FREE</span>
 
-          <div class="bottom clearfix">
-            <span class="icon-user"> 15442</span> 
-            <span class="icon-star"> 15202</span>
-            <span class="icon-arrow"> 100%</span>
+          <div class="bottom">
+            <div><span class="icon-user"></span><span>15442</span></div>
+            <div><span class="icon-star"></span><span>15202</span></div>
+            <div><span class="icon-arrow"></span><span>100%</span></div>
           </div>
 
           <span class="icon-star star"></span>
@@ -37,25 +35,38 @@ export default {
 
 <style lang="less" scoped>
 .row {
-  display: flex;
+  // display: inline-flex;
   margin: 5em 5em;
   .img {
-    flex: 1;
+    display: inline-block;
+    margin: 1em;
   }
-  .title {
-    flex: 1;
+  .detail {
+    display: inline-block;
+    height: 3em;
+    line-height: 3em;
+    .title {
+      display: inline-block;
+      width: 18em;
+      height: 1em;
+      line-height: 1em;
+      margin: 0;
+      font-size: 1.3rem;
+
+      color: #705db1;
+    }
+    .describe {
+    }
   }
-  .describe {
-    flex: 1;
-  }
+
   .provider {
-    flex: 1;
+    display: inline-block;
   }
   .price {
-    flex: 1;
+    display: inline-block;
   }
   .bottom {
-    flex: 1;
+    display: inline-block;
   }
 }
 </style>
