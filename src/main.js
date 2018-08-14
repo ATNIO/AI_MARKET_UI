06b87d2e77e168390df47921c860a01a44b1c0b4
junjia.wan <new_start@live.cn>
2018-08-14 11:41:46 +0800

    rebuild

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+import "normalize.css";
+
+import iView from "iview";
+import "iview/dist/styles/iview.css";
+
+Vue.use(iView);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
