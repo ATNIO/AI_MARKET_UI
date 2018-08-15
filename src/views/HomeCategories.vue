f000ff1d0b24d556436138d50ec4134850c83aae
mimo <931750817@qq.com>
2018-08-15 10:16:05 +0800

    home-catagories

--- src/views/HomeCategories.vue
+++ src/views/HomeCategories.vue
@@ -4,6 +4,8 @@
           <li
             v-for="(item, index) in filters"
             :key="index"
+            @click="_click(index)"
+            :class="{ active: index === current }"
           >
             <span>{{item}}</span>
           </li>
@@ -18,8 +20,14 @@ export default {
   name: "HomeCategories",
   data() {
     return {
-      filters
+      filters,
+      current: -1
     };
+  },
+  methods: {
+    _click(index) {
+      this.current = index;
+    }
   }
 };
 </script>
@@ -27,19 +35,25 @@ export default {
 <style lang="less" scoped>
 .home-categories {
   width: 290px;
-  height: 764px;
   background: #ffffff;
-  // box-shadow: 3px 0 10px 0 rgba(200, 199, 232, 0.5);
   box-shadow: 1px 0 10px 0 rgba(200, 199, 232, 0.2);
   .items {
-    margin-left: 20px;
     padding-top: 64px;
     font-family: SourceHanSansCN-Medium;
     font-size: 18px;
     color: #707070;
     list-style: none;
+    padding-bottom: 25px;
+
     li {
-      margin-bottom: 10px;
+      height: 40px;
+      padding-left: 20px;
+      line-height: 40px;
+      transition: all 0.2s ease;
+    }
+    .active {
+      color: #ffffff;
+      background: #797bf8;
     }
   }
 }
