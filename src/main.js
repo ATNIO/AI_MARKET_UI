76ebc84d3d37e400fb7fee5fa0315beac155cd8a
junjia.wan <new_start@live.cn>
2018-08-27 17:22:46 +0800

    add copy

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import api from "@/common/api"; // 导入api接口
 
 import { upperFirst, camelCase } from "lodash";
 import iView from "iview";
+import VueClipboard from "vue-clipboard2";
 
 import "normalize.css";
 import "iview/dist/styles/iview.css";
@@ -13,6 +14,7 @@ import "@/assets/styles/base.less";
 import "json-schema-view-js/dist/style.min.css";
 
 Vue.use(iView);
+Vue.use(VueClipboard);
 
 Vue.config.productionTip = false;
 
--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
 import * as types from "./types";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  plugins: [createPersistedState({ storage: window.sessionStorage })],
   state: {
     detailData: {},
     specification: {},
