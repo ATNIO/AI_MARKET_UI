585c449c6db48090f31288a941ee210f7ac60984
mimo <931750817@qq.com>
2018-09-05 18:35:18 +0800

    bottombar add href change icon

--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="bottom-bar">
     <div class="tp">
-      <a href="">
-        <Icon type="logo-github" size="40"/>
+      <a href="https://github.com/ATNIO">
+        <Icon custom="icon-github" size="40"/>
         <span>Github</span>
       </a>
-      <a href="">
-        <Icon type="ios-paper-plane" size="40"/>
+      <a href="https://t.me/ATN_Blockchain">
+        <Icon custom="icon-elegram" size="40"/>
         <span>Telegram</span>
       </a>
       <a href="">
-        <Icon type="ios-mail-open" size="40"/>
+        <Icon custom="icon-email" size="40"/>
         <span>Email</span>
       </a>
     </div>
